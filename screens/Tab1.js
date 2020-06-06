b1c9b7857471cbf47b1332045b2c04f34eb7a4c2
DanielCarrascoFalcon <30912090+DanielCarrascoFalcon@users.noreply.github.com>
2020-06-05 23:01:37 -0400

    Feature/map screen (#2)
    * Marker on Map
    
    * Current Location

--- screens/Tab1.js
+++ screens/Tab1.js
@@ -1,12 +1,45 @@
 import React from 'react'
-import { Text } from 'react-native'
+import MapView, { Marker } from 'react-native-maps';
+import { Text, StyleSheet, View } from 'react-native'
+import Layout from '../constants/Layout'
 
 const Tab1 = () => {
     return (
-        <Text>
-            Tab1
-        </Text>
+        <View style={styles.container}>
+            <MapView
+                style={styles.mapStyle}
+                initialRegion={{
+                    latitude: -33.499789,
+                    longitude: -70.613974,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            >
+                <Marker
+                    coordinate={{
+                        latitude: -33.499789,
+                        longitude: -70.613974,
+                    }}
+                    title='Hola'
+                    description='Primer marcador'
+                />
+            </MapView>
+        </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    mapStyle: {
+        width: Layout.window.width,
+        height: Layout.window.height,
+    },
+});
 
 export default Tab1;
