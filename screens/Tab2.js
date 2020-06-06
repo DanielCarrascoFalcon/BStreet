4dedaa9911236f59827490be1ce3efc9386ed7c9
Daniel Carrasco <drcarrasco@uc.cl>
2020-06-05 23:00:45 -0400

    Current Location

--- screens/Tab2.js
+++ screens/Tab2.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 const Tab2 = () => {
-    return (
-        <Text>
-            Tab2
-        </Text>
-    )
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    });
+
+    if (errorMsg) {
+        return (
+            <Text>Hubo un error: {errorMsg}</Text>
+        )
+    }
+    else if (location) {
+        text = JSON.stringify(location);
+        return (
+            <View>
+                <Text>Coordenadas actuales</Text>
+                <Text>
+                    Latitud: {location.coords.latitude}
+                </Text>
+                <Text>
+                    Longitud: {location.coords.longitude}
+                </Text>
+            </View>
+        )
+    }
+    else {
+        return (
+            <Text>Cargando...</Text>
+        )
+    }
+
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
 
 export default Tab2;
